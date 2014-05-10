d3909634f4b4975834b5f31b7cb33ae2d11cceac
Maxwell Xiong <mxiong@uchicago.edu>
2014-05-10 01:03:35 -0500

    JS for basic search

--- js/problem3.js
+++ js/problem3.js
@@ -1,4 +1,29 @@
 (function() {
 
+	var searchForm = document.getElementById('search_form');
+	var searchInput = document.getElementById('search_input');
+	var searchText = document.getElementById('search_text');
+	var resultMessage = document.getElementById('result_message');
+
+	searchForm.addEventListener('submit', handleSubmit);
+
+	function handleSubmit(evt) {
+		evt.preventDefault();
+		performSearch(searchInput.value);
+	}
+
+	function performSearch(query) {
+		var queryLength = query.length;
+		var matchIndicies = [];
+		var i = 0;
+		var matchIndex;
+
+		while ((matchIndex = searchText.innerText.indexOf(query, i)) > -1) {
+			matchIndicies.push(matchIndex);
+			i = matchIndex + queryLength;
+		}
+
+		console.log(matchIndicies);
+	}
 
 }());
