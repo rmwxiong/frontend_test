05bac36e8b9ce08ce780c08832fac0f804f2bbbf
Maxwell Xiong <mxiong@uchicago.edu>
2014-05-09 20:48:13 -0500

    Added slide data to js

--- js/problem1.js
+++ js/problem1.js
@@ -1,6 +1,35 @@
 (function() {
 
-	var rotator = document.getElementById('rotator');
-	console.log(rotator);
+	//////////////////
+	///DECLARATIONS///
+	//////////////////
 
+	var SLIDES = 
+	[{
+		imageUrl: '../img/kat_1.jpg',
+		title: 'Katarina',
+		caption: 'Fruitcake bonbon donut jelly-o brownie biscuit marzipan pie.'
+	},
+	{
+		imageUrl: '../img/kat_2.jpg',
+		title: 'Red Card Katarina',
+		caption: 'Cheesecake marzipan cupcake biscuit candy canes pudding.'
+	},
+	{
+		imageUrl: '../img/kat_3.jpg',
+		title: 'Kitty Cat Katarina',
+		caption: 'Applicake topping halvah lemon drops wypas cotton candy.'
+	},
+	{
+		imageUrl: '../img/kat_4.jpg',
+		title: 'High Command Katarina',
+		caption: 'Oat cake macaroon souffle cupcake chupa chups chocolate bar bear claw dragee lemon drops.'
+	},];
+
+	//////////////////
+	///////APP////////
+	//////////////////
+
+	var rotator = document.getElementById('rotator');
+	
 }());
