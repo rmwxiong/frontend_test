83e1c8f4b928fb672a5e2fa579373b8705349cf8
Maxwell Xiong <hododoho@gmail.com>
2014-05-20 21:16:23 -0500

    Don't use strict

--- js/problem1.js
+++ js/problem1.js
@@ -1,5 +1,3 @@
-'use strict';
-
 window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                               window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
                               (function( callback ){window.setTimeout(callback, 1000 / 60);});
